18368c30e80e6c2abab6013831d486133afff5b9
Fernando <fernando@agovest.com>
2022-01-18 20:16:18 -0600

    Configuring notifications push on Android Done

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,55 @@
 import 'package:fcm_app/screens/home_screen.dart';
 import 'package:fcm_app/screens/message_screen.dart';
+import 'package:fcm_app/services/push_notifications_service.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await PushNotificationService.initilizeApp();
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+
+  final GlobalKey<NavigatorState> navigatorKey = GlobalKey<NavigatorState>();
+  final GlobalKey<ScaffoldMessengerState> scaffoldKey = GlobalKey<ScaffoldMessengerState>();
+
+  @override
+  void initState() {
+    super.initState();
+    PushNotificationService.messagesStream.listen((message) { 
+
+      print('My app $message');
+
+      // To show data with SnackBar
+      final snackBar = SnackBar(content: Text(message));
+      scaffoldKey.currentState?.showSnackBar(snackBar);
+
+      // To redirect screen and sending arguments
+      navigatorKey.currentState?.pushNamed('messages', arguments: message);
+    });
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'NOTIFICACIONES',
       initialRoute: 'home',
+      navigatorKey: navigatorKey, // To navigate when user clicks the notification
+      scaffoldMessengerKey: scaffoldKey, // To show data ins Snackbarwhen user clicks the notification
       routes: {
         'home': (BuildContext context) => HomeScreen(),
         'messages': (BuildContext context) => MessageScreen(),
--- lib/services/push_notifications_service.dart
+++ lib/services/push_notifications_service.dart
@@ -1,1 +1,58 @@
 // 61:09:0C:3C:4B:72:7E:DE:C7:EC:B8:F6:FB:B1:72:09:F7:AE:11:5E
+
+import 'dart:async';
+
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
+
+class PushNotificationService {
+
+  static FirebaseMessaging messaging = FirebaseMessaging.instance;
+  static String? token;
+  static StreamController<String> _messageStream = StreamController.broadcast();
+
+  static Stream<String> get messagesStream => _messageStream.stream;
+
+  static Future<void> initilizeApp() async {
+    // push notifications
+    // To initialize Firebase app
+    await Firebase.initializeApp();
+    // Getting token
+    token = await FirebaseMessaging.instance.getToken();
+    print('Token: $token');
+
+    // Handlers
+    FirebaseMessaging.onBackgroundMessage(_backgroundHandler);
+    FirebaseMessaging.onMessage.listen(_onMessagedHandler);
+    FirebaseMessaging.onMessageOpenedApp.listen(_onMessageOpenApp);
+
+    // local Notifications
+  }
+
+  static Future<void> _backgroundHandler(RemoteMessage message) async {
+    // When the device gets a notification in Background
+    print('Background Handler ${message.messageId}');
+    _messageStream.add(message.notification?.title ?? 'No title');
+    // for message description
+    // _messageStream.add(message.notification?.body ?? 'No title');
+  }
+
+  static Future<void> _onMessagedHandler(RemoteMessage message) async {
+    // When the device gets a notification in Foreground
+    print('On Message Handler ${message.messageId}');
+    // Object Arguments key-value with message.data
+    // print(message.data);
+    _messageStream.add(message.data['product'] ?? 'No product');
+  }
+
+  static Future<void> _onMessageOpenApp(RemoteMessage message) async {
+    // When the user taps the notification
+    print('On Message OpenApp Handler ${message.messageId}');
+    _messageStream.add(message.data['product'] ?? 'No product');
+  }
+
+  static closeStreams() {
+    _messageStream.close();
+  }
+
+}
